b538c745f4b2e456e2689ed809b0d9a19d8342ad
Marek Fexa <marek.fexa@gmail.com>
2016-06-04 18:12:37 +0200

    Initial commit & experiments

---
+++ Consecutive.Console/PartitionExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consecutive.Console
+{
+    static class PartitionExtension
+    {
+        public static IEnumerable<Tuple<int, int>> Partition(this uint[] values)
+        {
+            if (values == null || values.Length == 0)
+            {
+                yield break;
+            }
+
+            int startIndex = 0;
+            for (int i = 1; i < values.Length; i++)
+            {
+                if (values[i] > values[i - 1] + 1)
+                {
+                    yield return Tuple.Create(startIndex, i - startIndex);
+                    startIndex = i;
+                }
+            }
+
+            yield return Tuple.Create(startIndex, values.Length - startIndex);
+        }
+    }
+}
---
+++ Consecutive.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consecutive.Console
+{
+    class Program
+    {
+        static void Main2(string[] args)
+        {
+            StringBuilder output= new StringBuilder();
+            using (StreamWriter sw = new StreamWriter("Test1.txt", false, Encoding.ASCII, 524288))
+            {
+                for (uint i = 0; i <= uint.MaxValue; i++)
+                {
+                    sw.Write($"{i} ");
+                    if (i == uint.MaxValue)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        
+        public static void Main(string[] args)
+        {
+            var values = new uint[] { 1, 2, 3, 18, 19, 24,25 };
+            foreach (var tuple in values.Partition())
+            {
+                System.Console.WriteLine(values[tuple.Item1] + "-" + tuple.Item2);
+                //Enumerable.Range(values[tuple.Item1], values[tuple.Item2]).Select
+
+
+            }
+        }
+    }
+}
